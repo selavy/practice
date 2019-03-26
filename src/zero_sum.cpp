49846ed393768541ec679c6dac4ba99b8eccaf98
Peter Lesslie <pclesslie@gmail.com>
2019-03-25 19:04:37 -0700

    ZeroSum solution #2

--- src/zero_sum.cpp
+++ src/zero_sum.cpp
@@ -3,8 +3,10 @@
 #include <utility>
 #include <algorithm>
 #include <unordered_map>
+#include <unordered_set>
 
-// naive solution: sort + linear scan
+// naive solution: sort + quadratic scan
+// Complexity: sort = O(n*log(n)) + quadratic scan = O(n^2) = O(n^2 + n*log(n))
 bool zero_sum1(std::vector<int> input) {
     std::sort(input.begin(), input.end());
     for (auto i = input.begin(); i != input.end(); ++i) {
@@ -28,7 +30,22 @@ bool zero_sum1(std::vector<int> input) {
             assert(sum < 0 && "loop invariant broken");
         }
     }
+    return false;
+}
 
+
+bool zero_sum2(std::vector<int> input) {
+    std::unordered_set<int> seen;
+    seen.insert(0);
+    int sum = 0;
+    for (int it : input) {
+        sum += it;
+        if (seen.find(sum) != seen.end()) {
+            return true;
+        } else {
+            seen.insert(sum);
+        }
+    }
     return false;
 }
 
@@ -39,6 +56,7 @@ TEST_CASE("Zero sum", "[zero sum]") {
         { { 1, 2, 3, -6 },                      true  },
         { { 6, 7, 2, -1, -4 },                  false },
         { { 1, 2, 3, 4, -4, -3, -2, -1 },       true  },
+        { { 0, 0, 0, 0, 1, 1, 1, 1 },           true  },
     };
 
     for (auto& tc : test_cases) {
@@ -47,6 +65,10 @@ TEST_CASE("Zero sum", "[zero sum]") {
 
         SECTION("ZeroSum1") {
             auto result = zero_sum1(input);
+            REQUIRE(result == expected);
+        }
+        SECTION("ZeroSum2") {
+            auto result = zero_sum2(input);
             REQUIRE(result == expected);
         }
     }
