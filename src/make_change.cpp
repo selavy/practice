3826f7a0b1c41e210a7fbcc83db7631671fc1bcd
Peter Lesslie <pclesslie@gmail.com>
2019-03-26 18:15:27 -0700

    Clean up test

--- src/make_change.cpp
+++ src/make_change.cpp
@@ -31,33 +31,21 @@ int make_change2(Iter first, Iter last, int value) {
 }
 
 struct TestCase {
-    TestCase(std::vector<int> il, int vv, int ww)
-        : coins(il), value(vv), ways(ww) {}
-
     std::vector<int> coins;
     int              value;
     int              ways;
 };
 
 TEST_CASE("Make change", "[change]") {
-    std::vector<std::vector<int>> coinsbags = {
-        { 1, 3, 5, 7 },
-        { 1, 2, 3 },
+    std::vector<TestCase> test_cases = {
+        { { 1, 3, 5, 7 }, 8, 6 },
+        { { 1, 2, 3 },    4, 4 },
     };
-    std::vector<std::pair<int, int>> test_cases = {
-        { 8, 6 },
-        { 4, 4 },
-    };
-    // std::vector<TestCase> test_cases = {
-    //     TestCase({ 1, 3, 5, 7 }, 8, 6),
-    //     TestCase({ 1, 2, 3 },    4, 4),
-    // };
 
-    // for (auto& tc : test_cases) {
     for (size_t i = 0; i < test_cases.size(); ++i) {
-        auto& coins = coinsbags[i];
-        auto value = test_cases[i].first;
-        auto ways = test_cases[i].second;
+        auto& coins = test_cases[i].coins;
+        auto value = test_cases[i].value;
+        auto ways = test_cases[i].ways;
 
         SECTION("Solution #1") {
             auto result = make_change1(coins.data(), coins.size(), value);
